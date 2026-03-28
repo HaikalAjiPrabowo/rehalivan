905398661bb8074d4b73aa0bf9a0ee45be433a81
ahmadzaidanmubarok3 <guybowo@gmail.com>
2026-03-28 23:19:58 +0700

    first commit

---
+++ frontend/learning-course-marketplace/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'learning-course-marketplace',
+  webDir: 'www'
+};
+
+export default config;
